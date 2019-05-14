40e51176e67eeedba12af5d6c297fafe6fed54f4
Damir <Damir02@hotmail.nl>
2019-05-14 14:29:07 +0200

    af

---
+++ resp.js
@@ -0,0 +1,9 @@
+function toggleNavPanel(x){
+  var panel = document.getElementById(x), maxH="110px";
+  if(panel.style.height == maxH){
+      panel.style.height = "0px";
+  } 
+    else {
+      panel.style.height = maxH;
+      }
+  }
